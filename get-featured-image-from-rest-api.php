c70230e3c96c8626fff35e654bbda426282ec833
Dale Nguyen <dungnq@itbox4vn.com>
2018-03-30 15:08:03 -0400

    Create get-featured-image-from-rest-api.php

---
+++ get-featured-image-from-rest-api.php
@@ -0,0 +1,22 @@
+// Put this code in functions.php or a separate plugin
+
+add_action('rest_api_init', 'register_rest_images' );
+
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
