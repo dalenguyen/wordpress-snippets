b7812e94f3f76ba16df06e5f5c9be931967f0f9e
Marcel Moll <marcel@watson-holmes.com>
2021-10-05 22:22:58 +0200

    Create embed_responsive_16_to_9_videos.php

---
+++ embed_responsive_16_to_9_videos.php
@@ -0,0 +1,28 @@
+function responsive_videos( $html ){
+    $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
+    return'<div class="embed-responsive embed-responsive-16by9">'.$html.'</div>';
+}
+
+add_filter( 'embed_oembed_html','evolution_wrap_oembed',10,1);
+
+//needed CSS Classes
+
+.embed-responsive.embed-responsive-16by9 {
+    position: relative;
+    padding-bottom: 56.25%; /* 16:9 */
+    padding-top: 25px;
+    height: 0;
+}
+
+.embed-responsive.embed-responsive-16by9 iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+video {
+    width: 100% !important;
+    height: auto !important;
+}
