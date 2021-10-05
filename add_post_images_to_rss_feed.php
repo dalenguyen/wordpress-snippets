c234b571ae451b89d568bf7e8290c85d8bbfd6ad
Marcel Moll <marcel@watson-holmes.com>
2021-10-05 21:52:23 +0200

    Create add_post_images_to_rss_feed.php

---
+++ add_post_images_to_rss_feed.php
@@ -0,0 +1,10 @@
+function add_post_image_to_rss($content) {
+    global $post;
+    if ( has_post_thumbnail( $post->ID ) ){
+        $content = '<div>' . get_the_post_thumbnail( $post->ID, 'large', array( 'style' => 'margin-bottom: 15px;' ) ) . '</div>' . $content;
+    }
+    return $content;
+}
+
+add_filter('the_excerpt_rss', 'add_post_images_to_rss');
+add_filter('the_content_feed', 'add_post_images_to_rss');
