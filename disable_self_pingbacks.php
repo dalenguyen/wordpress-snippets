d4c68274070a7e7b3039fce97c1d8ff85b5cf629
Marcel Moll <marcel@watson-holmes.com>
2021-10-05 21:28:38 +0200

    Create disable_self_pingback.php

---
+++ disable_self_pingbacks.php
@@ -0,0 +1,7 @@
+function disable_self_pingback( &$links ) {
+    $home = get_option( 'home' );
+    foreach ( $links as $l => $link )
+    if ( 0 === strpos( $link, $home ) )
+    unset($links[$l]);
+}
+add_action( 'pre_ping', 'disable_self_pingback' );
