4480378de53affa72248bd9438786e4b67c1aee4
Marcel Moll <marcel@watson-holmes.com>
2021-10-05 22:14:19 +0200

    Create add_additional_mime_types_to_media_libary.php

---
+++ add_additional_mime_types_to_media_libary.php
@@ -0,0 +1,12 @@
+function add_custom_mime_types($mimes){
+    $new_file_types = array (
+        'zip' => 'application/zip',
+        'pdf' => 'application/pdf',
+        'epub' => 'application/epub+zip',
+        //add more here
+    );
+
+    return array_merge($mimes,$new_file_types);
+}
+
+add_filter('upload_mimes','add_custom_mime_types');
