ef194a73be9a5a9ef991d7673c859cb8693c876f
Marcel Moll <marcel@watson-holmes.com>
2021-10-05 20:31:00 +0200

    Create get_random_post-usggestion_by_categories.php

---
+++ get_random_post_suggestion_by_categories.php
@@ -0,0 +1,17 @@
+//get categories of post
+$categories = get_the_terms( get_the_ID(), 'category' );
+
+//extract category id's from term objects
+$cat_ids = array_column($categories, 'term_id');
+
+//concat id's to a string with ',' as seperator
+$categories_string = implode($cat_ids, ',');
+
+//get five random posts that are in any category of the current post
+$posts = query_posts([
+    'post_not_in' => get_the_ID(),
+    'cat' => $categories_string,
+    'order' => 'ASC',
+    'orderby' => 'rand',
+    'post_per_page' => 5
+]);
