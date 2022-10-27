5dfa2f0b3a3c5c93b7dcef57b7d182fe7ed23d8e
Baris @ 1815 <baris@1815.nl>
2022-10-27 07:58:04 +0200

    refactor: remove unneeded extra variable in get_random_post_suggestion_by_tags.php

--- src/get_random_post_suggestion_by_tags.php
+++ src/get_random_post_suggestion_by_tags.php
@@ -1,12 +1,12 @@
 <?php
 //get tags of post
-$tags        = get_the_terms( get_the_ID(), 'tag' );
+$tags    = get_the_terms( get_the_ID(), 'tag' );
 
 //extract tag id's from term objects
-$tag_ids     = array_column( $tags, 'term_id' );
+$tag_ids = array_column( $tags, 'term_id' );
 
 //concat id's to a string with ',' as seperator
-$tags_string = implode( $tag_ids, ',' );
+$tag_ids = implode( $tag_ids, ',' );
 
 //get five random posts that are in any tag of the current post
 $posts = query_posts( [
