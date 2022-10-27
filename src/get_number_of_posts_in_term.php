f7f0c10b8ec43206ce9f48adc43d6d6d20968a8c
Baris @ 1815 <baris@1815.nl>
2022-10-27 07:34:53 +0200

    fix: change undefined method count + add check if term exists in get_number_of_posts_in_term.php

--- src/get_number_of_posts_in_term.php
+++ src/get_number_of_posts_in_term.php
@@ -10,4 +10,6 @@ $term->count;
 //Show the number of posts of any term by any identifier (e.g. for the term category and the term with id 1)
 
 $term = get_term_by( 'id', 1, 'category' );
-$term->count();
+if ( ! empty( $term ) ) {
+    echo $term->count;
+}
