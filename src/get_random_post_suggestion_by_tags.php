38c7d91474bc22f76924f208d200f589135d57b1
Baris @ 1815 <baris@1815.nl>
2022-10-27 08:08:41 +0200

    refactor: no need to change array to string for tag ids in get_random_post_suggestion_by_tags.php

--- src/get_random_post_suggestion_by_tags.php
+++ src/get_random_post_suggestion_by_tags.php
@@ -6,9 +6,6 @@ if ( ! empty( $tags ) ) {
     //extract tag id's from term objects
     $tag_ids = array_column( $tags, 'term_id' );
 
-    //concat id's to a string with ',' as seperator
-    $tag_ids = implode( ',', $tag_ids );
-
     //get five random posts that are in any tag of the current post
     $posts = get_posts( [
         'post_not_in'   => get_the_ID(),
