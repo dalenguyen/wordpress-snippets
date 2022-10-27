2995fa7552eee0c915e87f72eee4202b0e632e0c
Baris @ 1815 <baris@1815.nl>
2022-10-27 07:40:54 +0200

    refactor: check page exists before getting ID in get_page_id_from_page_title.php

--- src/get_page_id_from_page_title.php
+++ src/get_page_id_from_page_title.php
@@ -1,4 +1,6 @@
 <?php
 // This will return an object of the page
-$page    = get_page_by_title( 'page-name' );
-$page_id = $page->ID;
+$page = get_page_by_title( 'page-name' );
+if ( ! empty( $page ) ) {
+    $page_id = $page->ID;
+}
