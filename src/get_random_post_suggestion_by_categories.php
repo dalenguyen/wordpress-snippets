389edca279bb13b62643ad4c10e323d0c33a2dab
Baris @ 1815 <baris@1815.nl>
2022-10-27 07:55:36 +0200

    refactor: check if there are categories before proceeding in get_random_post_suggestion_by_categories.php

--- src/get_random_post_suggestion_by_categories.php
+++ src/get_random_post_suggestion_by_categories.php
@@ -2,17 +2,19 @@
 //get categories of post
 $categories = get_the_terms( get_the_ID(), 'category' );
 
-//extract category id's from term objects
-$cat_ids    = array_column( $categories, 'term_id' );
+if ( ! empty( $categories ) ) {
+    //extract category id's from term objects
+    $cat_ids    = array_column( $categories, 'term_id' );
 
-//concat id's to a string with ',' as seperator
-$cat_ids    = implode( ',', $cat_ids );
+    //concat id's to a string with ',' as seperator
+    $cat_ids    = implode( ',', $cat_ids );
 
-//get five random posts that are in any category of the current post
-$posts = get_posts( [
-    'post_not_in'   => get_the_ID(),
-    'cat'           => $cat_ids,
-    'order'         => 'ASC',
-    'orderby'       => 'rand',
-    'post_per_page' => 5
-] );
+    //get five random posts that are in any category of the current post
+    $posts = get_posts( [
+        'post_not_in'   => get_the_ID(),
+        'cat'           => $cat_ids,
+        'order'         => 'ASC',
+        'orderby'       => 'rand',
+        'post_per_page' => 5
+    ] );
+}
