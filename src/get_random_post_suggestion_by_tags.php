e9a79a072ae249979c6d590b5e010dc089482c9a
Baris @ 1815 <baris@1815.nl>
2022-10-27 07:59:08 +0200

    refactor: check if there are tags before proceeding in get_random_post_suggestion_by_tags.php

--- src/get_random_post_suggestion_by_tags.php
+++ src/get_random_post_suggestion_by_tags.php
@@ -2,17 +2,19 @@
 //get tags of post
 $tags    = get_the_terms( get_the_ID(), 'tag' );
 
-//extract tag id's from term objects
-$tag_ids = array_column( $tags, 'term_id' );
+if ( ! empty( $tags ) ) {
+    //extract tag id's from term objects
+    $tag_ids = array_column( $tags, 'term_id' );
 
-//concat id's to a string with ',' as seperator
-$tag_ids = implode( $tag_ids, ',' );
+    //concat id's to a string with ',' as seperator
+    $tag_ids = implode( $tag_ids, ',' );
 
-//get five random posts that are in any tag of the current post
-$posts = get_posts( [
-    'post_not_in'   => get_the_ID(),
-    'tag__in'       => $tags_string,
-    'order'         => 'ASC',
-    'orderby'       => 'rand',
-    'post_per_page' => 5
-] );
+    //get five random posts that are in any tag of the current post
+    $posts = get_posts( [
+        'post_not_in'   => get_the_ID(),
+        'tag__in'       => $tags_string,
+        'order'         => 'ASC',
+        'orderby'       => 'rand',
+        'post_per_page' => 5
+    ] );
+}
