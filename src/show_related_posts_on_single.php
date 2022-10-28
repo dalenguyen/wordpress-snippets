7e6c544af83f70c40c83af817f5dfcffbcdeebf1
tmdk <tmdk@users.noreply.github.com>
2022-10-28 04:59:53 +0200

    Related posts: fix WPCS violations (#25)
    Fixes WPCS violations, minor code smells and a typo in the related posts snippet

--- src/show_related_posts_on_single.php
+++ src/show_related_posts_on_single.php
@@ -1,10 +1,15 @@
 <?php
 
-// Show related posts @ single post
+// Show related posts @ single post.
 
+/**
+ * Show random posts from the same categories as the current post.
+ *
+ * @return void
+ */
 function related_posts() {
 
-	// current post id
+	// current post id.
 	$current_post_id = get_the_ID();
 
 	$related = new WP_Query(
@@ -12,7 +17,7 @@ function related_posts() {
 			'category__in'   => wp_get_post_categories( $current_post_id ),
 			'posts_per_page' => 4,
 			'orderby'        => 'rand',
-			'post__not_in'   => array( $current_post_id ) // exlcude currnet post
+			'post__not_in'   => array( $current_post_id ), // exclude current post.
 		)
 	);
 
@@ -20,22 +25,19 @@ function related_posts() {
 
 		echo '<div class="all-related-posts">';
 
-			while ( $related->have_posts() ) {  $related->the_post();
+		while ( $related->have_posts() ) {
+			$related->the_post();
 
-				// related post data
-				$post_id             = get_the_ID();
-				$post_title          = get_the_title( $post_id );
-				$post_permalink      = get_the_permalink( $post_id );
-				$post_featured_image = get_the_post_thumbnail( $post_id, 'medium' );
+			// entry div structure.
+			echo '<div class="related-post-item">';
+			echo '<div class="post-featured-image">';
+			the_post_thumbnail( 'medium' );
+			echo '</div>';
+			echo '<h3 class="post-title">' . esc_html( get_the_title() ) . '</h3>';
+			echo '<a href="' . esc_url( get_the_permalink() ) . '" class="button">Read More</a>';
+			echo '</div>';
 
-				// entry div structure
-				echo '<div class="related-post-item">';
-					echo '<div class="post-featured-image">' . $post_featured_image . '</div>';
-					echo '<h3 class="post-title">' . $post_title  . '</h3>';
-					echo '<a href="' . $post_permalink . '" class="button">Read More</a>';
-				echo '</div>';
-
-			}
+		}
 
 		echo '</div>';
 
